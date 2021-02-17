4a59a5985f4faa2a716e78270cbe3078983ec194
sethl <lewandowski.seth@gmail.com>
2021-02-16 18:10:35 -0700

    adding file

---
+++ forEachCodingExercise1.js
@@ -0,0 +1,31 @@
+// re-factor the below code to use a forEach helper function
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    for (var i = 0; i < posts.length; i++) {
+      savePost(posts[i]);
+    }
+}
+
+
+// re-factored code
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    posts.forEach(function(post) {
+    	savePost(post)
+    })
+}
+
+
+
+
+console.log(posts)
